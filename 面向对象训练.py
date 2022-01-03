c13afa69e75dba4fcc1132e1b13f24402970e374
xg <1437718371@qq.com>
2022-01-04 00:10:20 +0800

    实例对象

---
+++ 面向对象训练.py
@@ -0,0 +1,26 @@
+# 需求： 洗衣机， 功能：能洗衣服
+
+# 1、 定义洗衣机
+"""
+class 类名（）：
+    代码
+"""
+
+
+class Washer():
+    def wash(self):
+        print('能洗衣服')
+
+
+# 2、创建对象：
+"""
+对象名 = 类名（）
+"""
+haier = Washer()
+
+# 3、验证成果：
+
+# 打印haier对象
+print(haier)
+# 使用wash功能
+haier.wash()  # 对象方法/实例方法调用
